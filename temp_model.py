e6ef9d3b512f87aaf7095dc1365e7fedeec548a3
Neil Vora <nvora@chemium.local>
2020-10-13 21:43:09 -0500

    Model 3.0

---
+++ temp_model.py
@@ -0,0 +1,144 @@
+#coding=utf-8
+
+try:
+    import matplotlib.pyplot as plt
+except:
+    pass
+
+try:
+    import pandas as pd
+except:
+    pass
+
+try:
+    from sklearn.model_selection import train_test_split
+except:
+    pass
+
+try:
+    from hyperopt import Trials, STATUS_OK, tpe
+except:
+    pass
+
+try:
+    from hyperas.distributions import uniform
+except:
+    pass
+
+try:
+    from hyperas import optim
+except:
+    pass
+
+try:
+    from tensorflow.keras.models import Sequential
+except:
+    pass
+
+try:
+    from tensorflow.keras.layers import Conv2D
+except:
+    pass
+
+try:
+    from tensorflow.keras.layers import MaxPooling2D
+except:
+    pass
+
+try:
+    from tensorflow.keras.layers import Flatten
+except:
+    pass
+
+try:
+    from tensorflow.keras.layers import Dense
+except:
+    pass
+
+try:
+    from tensorflow.keras.layers import Dropout
+except:
+    pass
+
+try:
+    from tensorflow.keras.preprocessing import image
+except:
+    pass
+
+try:
+    from tensorflow.keras.utils import to_categorical
+except:
+    pass
+
+try:
+    from tensorflow.keras.applications.vgg19 import VGG19, preprocess_input, decode_predictions
+except:
+    pass
+
+try:
+    from sklearn.preprocessing import LabelEncoder
+except:
+    pass
+
+try:
+    from tqdm import tqdm
+except:
+    pass
+
+try:
+    import numpy as np
+except:
+    pass
+
+try:
+    from sklearn.model_selection import train_test_split
+except:
+    pass
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+
+from sklearn.model_selection import train_test_split
+# Split into Train/Test
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)    
+# Apply One-Hot Encoding for y-categorical variable
+label_encoder = LabelEncoder()
+label_encoder.fit(y_train)
+encoded_y_train = label_encoder.transform(y_train)
+encoded_y_test = label_encoder.transform(y_test)
+y_train_cat = to_categorical(encoded_y_train)
+y_test_cat = to_categorical(encoded_y_test) 
+# Define data inputs
+x_train = X_train
+y_train = y_train_cat
+x_test = X_test
+y_test = y_test_cat
+
+
+def keras_fmin_fnct(space):
+
+    model = Sequential()
+    model.add(Conv2D(26, kernel_size=(3, 3),padding="valid",activation='relu',input_shape=(28, 28, 3)))
+    model.add(Conv2D(64, (3, 3),padding="valid", activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(space['Dropout']))
+    model.add(Flatten())
+    #model.add(Dense(64, activation='relu'))
+    model.add(Dense(space['Dense']))
+    model.add(Activation(space['Activation']))
+    model.add(Dropout(space['Dropout_1']))
+    #model.add(Dropout(0.5))
+    model.add(Dense(26, activation='softmax'))    
+    # Compile Model
+    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+    
+    # Fit Model
+    model.fit(x_train, y_train, epochs=10,verbose=10, validation_data=(X_test, y_test_cat))
+    
+    return model
+
+def get_space():
+    return {
+        'Dropout': hp.uniform('Dropout', 0, 1),
+        'Dense': hp.choice('Dense', [256, 512, 1024]),
+        'Activation': hp.choice('Activation', ['relu', 'sigmoid']),
+        'Dropout_1': hp.uniform('Dropout_1', 0, 1),
+    }
