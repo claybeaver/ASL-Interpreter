1170e616e85ad522c5425a2c8b38fa1fbc2288c3
DESKTOP-MTL7LKS\Clay <claybeaver1991@gmail.com>
2020-10-15 13:32:00 -0500

    Cleaned Github Files / Fixed Webcam_Recognition.py to capture Segmented Region

--- Webcam_Recognition.py
+++ Webcam_Recognition.py
@@ -1,39 +1,47 @@
 ###################################
-##### Web Camera Interaction ######
+### Web Camera Hand Recognition ###
 ###################################
 
-
+###################################
 ### ********* NOTES *********** ###
 ### Spacebar = Save Screenshot ####
 ### Escape = Cancel Application ###
 ### *************************** ###
+###################################
 
-### In Progress: working on building a segmented to screenshot for the hand ###
 
 import cv2
 
 def show_webcam(mirror=False):
 
     cam = cv2.VideoCapture(0)
-    cv2.namedWindow("test")
-    img_counter = 0
+    img_counter = 1
 
     while True:
         ret, frame = cam.read()
         frame = cv2.flip(frame, 1)
-        if not ret:
-            print("~~~~~~~ Try Again ~~~~~~~")
-            break
-        cv2.imshow("test", frame)
+        cv2.line
+        
+        xmin = 384
+        xmax = 584
+        ymin = 100
+        ymax = 300
 
+        square = cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (0,0, 225), 3)
+        
+        if not ret:
+            print("~~~~~~~  Try Again  ~~~~~~~")
+            break
+        cv2.imshow("Hand Capture", frame)
+        
         k = cv2.waitKey(1)
         if k%256 == 27:  #--------------------> ESC pressed 
-            print("~~~~~~~ Closing Program ~~~~~~~")
+            print("~~~~~~~  Closing Program  ~~~~~~~")
             break
         elif k%256 == 32:  #------------------> SPACE pressed
-            img_name = "opencv_frame_{}.png".format(img_counter)
-            cv2.imwrite(img_name, frame)
-            print("~~~~~~~ {} has been saved ~~~~~~~".format(img_name))
+            img_name = "hand_capture_{}.png".format(img_counter)
+            cv2.imwrite(img_name, frame[ymin:ymax, xmin:xmax]) #------> Crop to image to ROI
+            print("~~~~~~~  {} has been saved  ~~~~~~~".format(img_name))
             img_counter += 1
 
     cam.release()
