0677efcfc69f866ccc79c390eef6eede332b090f
DESKTOP-MTL7LKS\Clay <claybeaver1991@gmail.com>
2020-10-16 09:36:01 -0500

    Cleaned code for variable decleration, added comments to help users.

--- Webcam_Recognition.py
+++ Webcam_Recognition.py
@@ -15,41 +15,37 @@ import base64
 
 def show_webcam(mirror=False):
 
-    cam = cv2.VideoCapture(0)
-    # img_counter = 1               #-------------> Used for saving images to local computer
+    cam = cv2.VideoCapture(0)                       #-------------> Set to 0 (for using camera) or 1 (if using built in webcam)
+    # img_counter = 1                               #-------------> Used for saving images to local computer
 
     while True:
         ret, frame = cam.read()
-        frame = cv2.flip(frame, 1)  #-------------> To activate mirror imaging
+        frame = cv2.flip(frame, 1)                  #-------------> To activate mirror imaging
         
-        xmin = 384
-        xmax = 584
-        ymin = 100
-        ymax = 300
-
-        xmin2 = 56
-        xmax2 = 256
-        ymin2 = 100
-        ymax2 = 300
+        (xmin, xmax, ymin, ymax) = (384, 584, 100, 300)
+        
+        (xmin2, xmax2, ymin2, ymax2) = (56, 256, 100, 300)
 
         cv2.rectangle(frame, (xmin-2,ymin-2), (xmax+2,ymax+2), (0,0, 225), 2)
         
         if not ret:
-            print("~~~~~~~  Try Again  ~~~~~~~")
+            print("~~~~~~~~~~~~           Try Again            ~~~~~~~~~~~~")
             break
         cv2.imshow("Hand Capture", frame)
         
         k = cv2.waitKey(1)
-        if k%256 == 27:  #--------------------> ESC pressed 
-            print("~~~~~~~~~~~~  Terminating Program  ~~~~~~~~~~~~")
+        if k%256 == 27:                            #--------------------> ESC pressed 
+            print("~~~~~~~~~~~~       Terminating Program      ~~~~~~~~~~~~")
             break
-        elif k%256 == 32:  #------------------> SPACE pressed
+        elif k%256 == 32:                          #------------------> SPACE pressed
             retval, image = cam.read()
             cropped = image[ymin2:ymax2, xmin2:xmax2]
             cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
             retval, buffer = cv2.imencode('.jpg', cropped)
             jpg_as_text = base64.b64encode(buffer)
-            print(jpg_as_text) #-----------------> Test print for base64 string   #\
+            print("~~~~~~~~~~~~ Image Encoded to Base64 Format ~~~~~~~~~~~~")
+            print(jpg_as_text)                     #-----------------> Test print for base64 string 
+            print("~~~~~~~~~~~~ Image Encoded to Base64 Format ~~~~~~~~~~~~")  
             # img_name = "hand_capture_{}.jpg".format(img_counter)                # \
             # cv2.imwrite(img_name, frame[ymin:ymax, xmin:xmax])                  #  }----> Code to save image on local computer
             # print("~~~~~~~  {} has been saved  ~~~~~~~".format(img_name))       # /
