466878b06adf7f6be5e903fa40a98abf9d16d7da
Neil Vora <nvora@chemium.local>
2020-10-16 00:32:06 -0500

    Added index.html & flask code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response
-from Webcam_Recognition import main
+from Webcam_Recognition import show_webcam
 
 app = Flask(__name__)
 
@@ -13,5 +13,14 @@ def gen(show_webcam):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(show_webcam()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
+if __name__ == '__main__':
+    # defining server ip address and port
+    app.run(host='0.0.0.0',port='5000', debug=True)
