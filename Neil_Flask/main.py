d7b104d9a29c621fc73321f9dd1294d0e4362f02
Neil Vora <nvora@chemium.local>
2020-10-16 19:36:12 -0500

    Neil Flask changes

--- Neil_Flask/main.py
+++ Neil_Flask/main.py
@@ -1,23 +1,22 @@
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from Webcam_Recognition import VideoCamera
+import cv2
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def camera():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/image', methods=['GET','POST'])
+def signup():
+    print('Test')
+    if request.method == 'POST':
+        return jsonify(request.form['userID'], request.form['file'])
+    return render_template('image.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
