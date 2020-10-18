7de8f2160938c4fba0ae9388be162abedb594cc0
Neil Vora <nvora@chemium.local>
2020-10-17 21:00:19 -0500

    Moved proper main.py file into Overall Test folder

--- Overall Test/main.py
+++ Overall Test/main.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template, Response
-from Webcam_Recognition import VideoCamera
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from flask import Flask, render_template, Response, request, jsonify
+from Prediction import predict
+
 app = Flask(__name__)
+
+
 @app.route('/')
-def index():
-    # rendering webpage
+def camera():
     return render_template('index.html')
-def gen(camera):
-    while True:
-        #get camera frame
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/image', methods=['GET', 'POST'])
+def signup():
+    incoming_pkg = request.get_json()
+    img_b64_str_encoded = str(incoming_pkg['imgBase64'])
+    image_array = img_b64_str_encoded.split(',')
+    image = image_array[len(image_array) - 1]
+    app.logger.info("Processing and Predicting Which Digit ...")
+    prediction = predict(image)
+
+    # prediction = int(img_predict(img_b64_str_encoded))
+    # response = f'{prediction}'
+    # app.logger.info(f'Received prediction :: {jsonify(prediction=response)}')
+
+    return jsonify(response=prediction)
+
+
 if __name__ == '__main__':
-    # defining server ip address and port
-    app.run(host='0.0.0.0',port='5000', debug=True)
+    app.run(host='0.0.0.0', debug=True)
