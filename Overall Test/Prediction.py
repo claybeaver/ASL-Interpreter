e0d7f30ddc6591d9ecd95a4dbfa120c9233a68e3
p4rc1v4l <mlastudy.riceu@gmail.com>
2020-10-17 22:48:42 -0500

    preciting not correctly but predicting and sending to js

--- Overall Test/Prediction.py
+++ Overall Test/Prediction.py
@@ -1,18 +1,22 @@
 import base64
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+from sklearn.preprocessing import LabelEncoder
+
 import io
-from PIL import image
-
-
-
+import PIL
 
 
 def predict(base64string):
+    train_image = []
     imgdata = base64.b64decode(base64string)
-    image = PIL.Image.open(io.BytesIO(imgdata))
-    img = image.load_img(image, target_size = (100,100,3), color_mode = "rgb")
+    img = PIL.Image.open(io.BytesIO(imgdata))
+    # img = image.load_img(img, target_size = (100,100,3), color_mode = "rgb")
+    img = img.resize((200, 200)).convert(mode='L')
     img = image.img_to_array(img)
     img = img/255
     train_image.append(img)
@@ -20,6 +24,15 @@ def predict(base64string):
 
     model = load_model('model_1.h5', compile = True)
 
+    images_names = pd.read_csv('../Image Values2.csv')
+    labels = images_names['Image Value'].values
+    label_encoder = LabelEncoder()
+    label_encoder.fit(labels)
+
     encoded_predictions = model.predict_classes(X[:])
     prediction_labels = label_encoder.inverse_transform(encoded_predictions)
+    print('######## PREDICTION #########')
+    print(prediction_labels)
+    print('######## PREDICTION #########')
+    return prediction_labels[0]
 
